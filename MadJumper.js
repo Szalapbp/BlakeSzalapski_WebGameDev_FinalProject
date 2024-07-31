a33a245fa6c673bb77c66eecff4797bb834e5877
Szalapbp <szalapbp@mail.uc.edu>
2024-07-30 21:10:29 -0400

    MadJumper.js update
    created randomly generated platforms, a growing max world height to simulate infinite world, and created a camera movement that follows the players highest point, if the player falls the camera doesn't follow

--- MadJumper.js
+++ MadJumper.js
@@ -1,4 +1,9 @@
 console.log('script is running');
+let initialWorldHeight = 640 * 10;
+let worldHeight = initialWorldHeight;
+let worldTop = 0;
+let highestY = 0;
+
 const config ={
     type: Phaser.AUTO,
     parent: 'game', 
@@ -16,7 +21,7 @@ const config ={
     physics:{
         default: "arcade",
         arcade: {
-            gravity:{y: 300}
+            gravity:{y: 1000}
         }
     }
 };
@@ -25,12 +30,20 @@ const game = new Phaser.Game(config);
 let player;
 let enemy;
 let egg;
-let rngPlatform;
-let platform;
+let startingPlatform;
+let platforms;
 let cursors;
 let keys ={};
+let camera;
 let gameStarted = false;
 let gameOverText;
+let platformHeight = 20;
+let spacing = 150;
+let lastPlatformY = 0;
+let previousY = 0;
+
+
+
 
 
 function preload(){
@@ -46,11 +59,15 @@ function create(){
         'goose'
     );
 
-    platform = this.physics.add.staticSprite(
+    startingPlatform = this.physics.add.staticSprite(
         this.physics.world.bounds.width / 2,
         this.physics.world.bounds.height - 10,
         'platform'
     )
+
+    platforms = this.physics.add.staticGroup();
+
+    createInitialPlatforms.call(this);
 
     
     player.setCollideWorldBounds(true);
@@ -58,12 +75,55 @@ function create(){
     cursors = this.input.keyboard.createCursorKeys();
     keys.a = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
     keys.d = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
-    this.physics.add.collider(player, platform, playerBounce, null, this); 
+    this.physics.add.collider(player, startingPlatform, playerBounce, null, this); 
+    this.physics.add.collider(player, platforms, playerBounce, null, this);
+
+    camera = this.cameras.main;
+    camera.setBounds(0, 0, config.width, worldHeight);
+    
+
+    previousY = player.y
 
 };
 
-function playerBounce(player){
-    player.setVelocityY(-300);
+function playerBounce(player, platform){
+    player.setVelocityY(-550);
+}
+
+function createInitialPlatforms(){
+    lastPlatformY = config.height - spacing;
+    for(let i = 0; i < 3; i++){
+        let x = Phaser.Math.Between(50, config.width - 50);
+        let y = lastPlatformY;
+        addPlatform.call(this, x, y);
+    }
+   
+}
+
+function addPlatform(x, y, width = 30){
+    let platform = platforms.create(x, y, 'platform');
+    let scale = width / platform.width;
+    platform.setScale(scale, 1);
+    platform.body.setOffset(0, 0)
+    platform.refreshBody();
+    platform.setSize(width, platformHeight);
+    platform.setImmovable(true);
+    platform.body.allowGravity = false;
+    platform.setVisible(true);
+    lastPlatformY = y;
+    return platform;
+
+}
+
+function extendWorldBounds() {
+   let extendAmount = config.height;
+   worldTop -= extendAmount;
+   worldHeight += extendAmount;
+
+   this.physics.world.setBounds(0, worldTop, config.width, worldHeight);
+   camera.setBounds(0, worldTop, config.width, worldHeight);
+
+   addNewPlatforms.call(this);
 }
 
 function update(){
@@ -78,4 +138,48 @@ if(cursors.left.isDown || keys.a.isDown){
     player.body.setVelocityX(-200);
 }
 
-};
+
+ let targetCameraY = highestY - 400;
+    camera.scrollY = targetCameraY;
+
+    console.log(`Highest Y: ${highestY}`);
+    console.log(`player y: ${player.y}`);
+
+    if (player.y < highestY || highestY === 0){
+        highestY = player.y;
+    }
+
+if (player.y < camera.scrollY + config.height / 4 ) {
+    extendWorldBounds.call(this);
+}
+
+let currentMaxPlatformY = platforms.getChildren().reduce((max, platform) => Math.max(max, platform.y), 0);
+if (player.y < currentMaxPlatformY - spacing) {
+    extendWorldBounds.call(this);
+}
+
+previousY = player.y;
+}
+
+
+
+function addNewPlatforms(){
+let currentY = lastPlatformY;
+while (currentY > worldTop){
+    let platformCount = Phaser.Math.Between(1, 2);
+    let platformPositions =[];
+
+    for(let i = 0; i < platformCount; i++){
+        let x;
+        do{
+            x = Phaser.Math.Between(50, config.width - 50);
+        }
+        while (platformPositions.some(pos => Math.abs(pos - x) < 100));
+        platformPositions.push(x);
+        addPlatform.call(this, x, currentY);
+    }
+    currentY -= spacing
+}
+lastPlatformY = currentY + spacing;
+}
+
